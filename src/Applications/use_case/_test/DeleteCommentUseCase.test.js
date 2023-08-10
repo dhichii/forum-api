b134dcb61f4ff74f93e801304c80eea7727aca6c
Adhicitta Masran <adhicittamasran@gmail.com>
2023-08-10 18:31:30 +0800

    add DeleteCommentUseCase test

---
+++ src/Applications/use_case/_test/DeleteCommentUseCase.test.js
@@ -0,0 +1,36 @@
+const CommentRepository =
+    require('../../../Domains/comments/CommentRepository');
+const DeleteCommentUseCase = require('../DeleteCommentUseCase');
+
+describe('DeleteCommentUseCase', () => {
+  it('should orchestrating the delete comment action correctly', async () => {
+    const useCasePayload = {
+      commentId: 'comment-123',
+      threadId: 'thread-123',
+      userId: 'user-123',
+    };
+
+    const mockCommentRepository = new CommentRepository();
+
+    mockCommentRepository.verifyAvailableCommentInThread = jest.fn()
+        .mockImplementation(() => Promise.resolve());
+    mockCommentRepository.verifyCommentOwner = jest.fn()
+        .mockImplementation(() => Promise.resolve());
+    mockCommentRepository.deleteCommentById = jest.fn()
+        .mockImplementation(() => Promise.resolve());
+
+    const deleteCommentUseCase = new DeleteCommentUseCase({
+      commentRepository: mockCommentRepository,
+    });
+
+    const result = await deleteCommentUseCase.execute(useCasePayload);
+
+    expect(result).toBeUndefined();
+    expect(mockCommentRepository.verifyAvailableCommentInThread)
+        .toBeCalledWith(useCasePayload.commentId, useCasePayload.threadId);
+    expect(mockCommentRepository.verifyCommentOwner)
+        .toBeCalledWith(useCasePayload.commentId, useCasePayload.userId);
+    expect(mockCommentRepository.deleteCommentById)
+        .toBeCalledWith(useCasePayload.commentId);
+  });
+});
