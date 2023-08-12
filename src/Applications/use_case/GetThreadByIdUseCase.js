ea81103932ee8e26d11284a82241c3a895aaa143
Adhicitta Masran <adhicittamasran@gmail.com>
2023-08-12 22:57:09 +0800

    add replies to GetThreadByIdUseCase

--- src/Applications/use_case/GetThreadByIdUseCase.js
+++ src/Applications/use_case/GetThreadByIdUseCase.js
@@ -1,12 +1,15 @@
 class GetThreadByIdUseCase {
-  constructor({threadRepository, commentRepository}) {
+  constructor({threadRepository, commentRepository, replyRepository}) {
     this._threadRepository = threadRepository;
     this._commentRepository = commentRepository;
+    this._replyRepository = replyRepository;
   }
 
   async execute(id) {
     const thread = await this._threadRepository.getThreadById(id);
     let comments = await this._commentRepository.getCommentsByThreadId(id);
+    const replies = await this._replyRepository.getRepliesByThreadId(id);
+
     comments = comments.map((comment) => ({
       id: comment.id,
       username: comment.username,
@@ -14,6 +17,15 @@ class GetThreadByIdUseCase {
       content: comment.is_deleted ?
         '**komentar telah dihapus**' :
         comment.content,
+      replies: replies.filter((reply) => reply.comment_id === comment.id)
+          .map((reply) => ({
+            id: reply.id,
+            content: reply.is_deleted ?
+              '**balasan telah dihapus**' :
+              reply.content,
+            date: reply.date,
+            username: reply.username,
+          })),
     }));
 
     return {...thread, comments};
